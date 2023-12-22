5fea181f652a350170e134826038e14f8a9ef84e
Ashutosh Kumar Roy <ashu00619@gmail.com>
2023-12-22 15:15:30 +0530

    django rest_framework setup done

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Diary
+
 
 # Create your views here.
+@api_view(["GET"])
+def getRoutes(request):
+    return Response("hello api")
